7fd673bdad1b9b0a0cadf04932e191b504c0f25e
Joe <hutcheson2001@yahoo.com>
2022-01-29 18:00:22 -0500

    Working on the like count

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,8 @@
 const counter = document.getElementById("counter")
-let number = counter.textContent
+let number = parseInt(counter.textContent, 10)
 
-// setInterval(addSecond, 1000) 
 
+let counterInt = setInterval(addSecond, 1000) 
 
 
 let minusButton = document.getElementById("minus")
@@ -12,13 +12,67 @@ let plusButton = document.getElementById("plus")
   plusButton.addEventListener("click", addSecond)
 
 let heartButton = document.getElementById("heart")
+  heartButton.addEventListener("click", likeNumber)
+  
 
 let pauseButton = document.getElementById("pause")
+  pauseButton.addEventListener("click", handlePause)
+
 
 function addSecond(){
-  counter.textContent = number ++
+  number ++
+  counter.textContent = number
 }
 
 function minusSecond(){
-  counter.textContent = number --
+  number --
+  counter.textContent = number 
+}
+let heartClickCount = '0'
+
+function likeNumber(){
+  let list = document.getElementById("list");
+  let p = document.createElement("p");
+  p.setAttribute('id', `${number}`)
+  console.log(p)
+  let numberCount = document.getElementById(`${number}`)
+  console.log(numberCount)
+  // list.appendChild(numberCount)
+  heartClickCount++
+  numberCount.textContent = `${heartClickCount}likes for ${number}.`
+}
+
+function handlePause(){
+  if (pauseButton.textContent === " pause ")
+  //pausing
+  {pauseButton.textContent = "resume";
+  stopTimer();
+  disableButtons();
+}
+  ///resuming 
+    else {pauseButton.textContent = " pause ";
+    startTimer();
+    enableButtons()
+    
+  }
+}
+
+function stopTimer(){
+  clearInterval(counterInt)
+}
+
+function startTimer(){
+  setInterval(addSecond, 1000)
+}
+
+function disableButtons(){
+  plusButton.removeEventListener("click", addSecond);
+  minusButton.removeEventListener("click", minusSecond);
+  heartButton.removeEventListener("click", likeNumber);
+}
+
+function enableButtons(){
+  plusButton.addEventListener("click", addSecond);
+  minusButton.addEventListener("click", minusSecond);
+  heartButton.addEventListener("click", likeNumber);
 }
